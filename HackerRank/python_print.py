2eef3b6c7def08d31cbf916bedb11008a08785c3
Marlon Ramirez <marlon.ramirezlizcano@gmail.com>
2022-07-29 23:37:06 -0500

    python-print problem solved from HackerRank

---
+++ HackerRank/python_print.py
@@ -0,0 +1,13 @@
+def number_row(number) :
+    for i in range(1,number+1):
+        print(i, end="")
+
+
+
+
+
+if __name__ == '__main__':
+    number = int(input("Write a number: "))
+    number_row(number)
+
+
